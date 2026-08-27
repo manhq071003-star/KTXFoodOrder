3b657bccb20273cdd55dceb7ae9425fb52bcacd9
Aemon-coder <23010595@st.phenikaa-uni.edu.vn>
2026-08-27 20:46:23 +0700

    code class student

--- backend/src/main/java/model/Student.java
+++ backend/src/main/java/model/Student.java
@@ -1,4 +1,47 @@
 package model;
 
-public class Student {
+public class Student extends User {
+
+    private String dormitoryAddress;
+    private double balance;
+
+    public Student() {
+        super();
+    }
+
+    public Student(String id, String fullName, String phone,
+                   String dormitoryAddress, double balance) {
+
+        super(id, fullName, phone);
+
+        this.dormitoryAddress = dormitoryAddress;
+        this.balance = balance;
+    }
+
+    public String getDormitoryAddress() {
+        return dormitoryAddress;
+    }
+
+    public void setDormitoryAddress(String dormitoryAddress) {
+        this.dormitoryAddress = dormitoryAddress;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + getId() + '\'' +
+                ", fullName='" + getFullName() + '\'' +
+                ", phone='" + getPhone() + '\'' +
+                ", dormitoryAddress='" + dormitoryAddress + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
 }
