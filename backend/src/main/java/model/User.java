bdbda626d45829edfa9b2e268d01601e6bc2d5cd
Aemon-coder <23010595@st.phenikaa-uni.edu.vn>
2026-08-27 20:31:34 +0700

    code class user

--- backend/src/main/java/model/User.java
+++ backend/src/main/java/model/User.java
@@ -1,4 +1,50 @@
 package model;
 
 public class User {
+
+    private String id;
+    private String fullName;
+    private String phone;
+
+    public User() {
+    }
+
+    public User(String id, String fullName, String phone) {
+        this.id = id;
+        this.fullName = fullName;
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
 }
