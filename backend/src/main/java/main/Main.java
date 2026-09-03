e3650a942f78d47e7397c82b03019a12d6293478
DoXuanNghia <xuannghiado2006@mail.com>
2026-09-03 22:25:22 +0700

    Do-Xuan-Nghia food code

--- backend/src/main/java/main/Main.java
+++ backend/src/main/java/main/Main.java
@@ -1,9 +1,47 @@
 package main;
 
+import com.sun.net.httpserver.HttpServer;
+import config.Database;
+import handler.*;
+
+import java.awt.Desktop;
+import java.io.File;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.URI;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // TODO: Khởi tạo CSDL, khởi tạo HttpServer 8080 và đăng ký các Handler
+        Database.initDatabase();
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/api/foods", new FoodHandler());
+        server.createContext("/api/students", new StudentHandler());
+        server.createContext("/api/cart", new CartHandler());
+        server.createContext("/api/orders", new OrderHandler());
+        server.createContext("/api/payment", new PaymentHandler());
+
+        String frontendDir = "frontend";
+        if (!new File(frontendDir).exists()) {
+            frontendDir = "../frontend";
+        }
+        server.createContext("/", new StaticFileHandler(frontendDir));
+
+        server.setExecutor(null);
+        server.start();
+
+        String appUrl = "http://localhost:8080/index.html";
+        System.out.println("🚀 KTX FoodExpress Server Running at: " + appUrl);
+
+        try {
+            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                Desktop.getDesktop().browse(new URI(appUrl));
+            } else {
+                Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + appUrl);
+            }
+        } catch (Exception e) {
+            System.out.println("Mở trình duyệt thủ công tại: " + appUrl);
+        }
     }
 }
