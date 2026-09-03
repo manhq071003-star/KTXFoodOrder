483783cb441cf78fd802c48cd5ec416516c2bdea
phantunglam3327 <phtlam3327@gmail.com>
2026-09-03 21:25:51 +0700

    update project

--- backend/src/main/java/service/BankTransferPayment.java
+++ backend/src/main/java/service/BankTransferPayment.java
@@ -3,7 +3,6 @@ package service;
 public class BankTransferPayment implements PaymentMethod {
     @Override
     public boolean processPayment(double amount) {
-        // TODO: Xử lý thanh toán chuyển khoản QR
-        return false;
+        return true;
     }
 }
--- backend/src/main/java/service/WalletPayment.java
+++ backend/src/main/java/service/WalletPayment.java
@@ -3,7 +3,6 @@ package service;
 public class WalletPayment implements PaymentMethod {
     @Override
     public boolean processPayment(double amount) {
-        // TODO: Xử lý trừ tiền Ví KTX
-        return false;
+        return true;
     }
 }
